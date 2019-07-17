7cf1f1b824e4a713d1b85de91516d46f5082ee37
pjs <pjseoane@gmail.com>
2019-07-17 20:42:20 -0300

    1st Commit paulinux

---
+++ ROFEXmaven/src/main/java/com/qaant/rofexmaven/MainAccess.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qaant.rofexmaven;
+
+/**
+ * * 
+ * @author pjseoane@qaantcap.com
+ */
+public class MainAccess {
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+             
+            loginREST newConnection = new loginREST();
+            System.out.println("\nTOKEN->>>>>>>>>>:"+newConnection.getToken());
+            
+            String instruments =newConnection.getInstruments();
+            System.out.println("Elements "+instruments);
+            
+            String instDetails =newConnection.getInstrumentDetail("RFX20Jun19");
+            System.out.println("Detail "+instDetails);
+        
+    }
+}
